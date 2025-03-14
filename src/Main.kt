11e2d939d00e27d6bca339252af1561a7aceaf4b
prik-ie <adekpenjahitan08@gmail.com>
2025-03-14 09:55:41 +0700

    don

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    list()
+    beefhouse()
+    salerobasamo()
+}
+
+fun list () {
+    val nama = "PILIHAN LAUK:"
+
+    println(nama)
+}
+
+fun beefhouse () {
+    var barbeque = "-beef black pepper"
+
+    println(barbeque)
+
+}
+
+fun salerobasamo (){
+    var gulaiasam = "-ikan kakap jumbo"
+
+    print(gulaiasam)
+}
---
+++ src/kuis1.kt
@@ -0,0 +1,24 @@
+fun main() {
+isimulut()
+    mulut()
+}
+
+fun isimulut () {
+    var gigi =  'a'
+    println(gigi)
+}
+fun mulut (){
+    var org = mutableMapOf(
+        "lidah" to "ldh",
+        "kerongkongan" to "krgkgn",
+        "gig" to "gg"
+    )
+
+    org ["lidah"]= "a"
+
+    println(org)
+}
+
+
+
+
